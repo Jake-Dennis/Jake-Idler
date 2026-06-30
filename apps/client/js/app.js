d7e7b6b7685ce591b23357615963e4132c4617b6
Jake-Dennis <Jakepauldennis@hotmail.com>
2026-06-30 17:08:45 +0930

    fix: multiply updateAbDisplay ATK by 2 to match difficulty curve

--- apps/client/js/app.js
+++ apps/client/js/app.js
@@ -3212,7 +3212,7 @@ function updateAbDisplay() {
   var rarityLabel = pos <= 2 ? 'common' : pos <= 5 ? 'uncommon' : 'rare';
   var gearLv = Math.ceil(floor / 10) * 10;
   var bracket = Math.max(1, Math.ceil(floor / 10));
-  var avgAtk = Math.round((c * Math.round(weapBase + ((gearLv - 10) / 10) * 300 + (rb.common||0)) + u * Math.round(weapBase + ((gearLv - 10) / 10) * 300 + (rb.uncommon||0)) + r * Math.round(weapBase + ((gearLv - 10) / 10) * 300 + (rb.rare||0))) / slots);
+  var avgAtk = Math.round((c * Math.round(weapBase + ((gearLv - 10) / 10) * 300 + (rb.common||0)) + u * Math.round(weapBase + ((gearLv - 10) / 10) * 300 + (rb.uncommon||0)) + r * Math.round(weapBase + ((gearLv - 10) / 10) * 300 + (rb.rare||0))) / slots) * 2;
   var monDef = Math.round(monBaseDef * bracket);
   var monHp = Math.round(monBaseHp * bracket);
 
